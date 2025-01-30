85a046a163b95e880f7537de6202d18e0128b7d3
TheHypnotic <72342378+TheHypnotic@users.noreply.github.com>
2025-01-30 14:42:21 +0330

    Update gcb-main.py
    Deprecated Lib was corrected
    from datasets import ClassLabel, load_dataset
    from evaluate import load as load_metric
    
    line 393 was commenetd

--- sketcher/gcb-main.py
+++ sketcher/gcb-main.py
@@ -10,7 +10,9 @@ from typing import Optional
 
 import datasets
 import numpy as np
-from datasets import ClassLabel, load_dataset, load_metric
+# from datasets import ClassLabel, load_dataset, load_metric
+from datasets import ClassLabel, load_dataset
+from evaluate import load as load_metric
 
 import transformers
 from transformers import (
@@ -388,7 +390,7 @@ def main():
   data_collator = DataCollatorForTokenClassification(tokenizer, pad_to_multiple_of=8 if training_args.fp16 else None)
 
   # Metrics
-  metric = load_metric("eval.py")
+  # metric = load_metric("eval.py")
 
   def compute_metrics(p):
     predictions, labels = p
